d94a8917e36a7732cd2a8cdc30942d244fade3e1
Holy-Morphism <ranaadeeltahir@gmail.com>
2024-03-11 21:05:02 +0500

    done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        JetBrainsMono: ["JetBrainsMono"],
+      },
+    },
   },
   plugins: [],
 };
