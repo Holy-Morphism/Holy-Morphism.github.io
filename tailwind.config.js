5ae0ea3b5eb69088c0a762c055f3ece091dfbb15
Holy-Morphism <ranaadeeltahir@gmail.com>
2024-03-12 13:17:51 +0500

    font added along with fallback font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        JetBrainsMono: ["JetBrainsMono"],
+        JetBrainsMono: ["JetBrainsMono", "sans-serif"],
       },
     },
   },
