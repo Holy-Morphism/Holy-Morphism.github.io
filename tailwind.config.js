7c697acbc173f35cf6e8e6deb09d6f449535319b
Holy-Morphism <ranaadeeltahir@gmail.com>
2024-03-14 22:01:10 +0500

    project card add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      Primary: "#070F2B",
+      Secodary: "#1B1A55",
+      TertiaryColor1: "#535C91",
+      TertiaryColor2: "#9290C3",
+    },
     extend: {
       fontFamily: {
         JetBrainsMono: ["JetBrainsMono", "monospace"],
